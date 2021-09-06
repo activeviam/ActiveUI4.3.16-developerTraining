cb0b85f3ed57a859c7ed33c0cae221b604ac618e
Clement Caylux <cca@activeviam.com>
2021-09-06 20:31:30 +0200

    specify  exercise number in TODOs

--- src/components/drawers/dashboards/Dashboards.tsx
+++ src/components/drawers/dashboards/Dashboards.tsx
@@ -5,7 +5,7 @@ import {Container} from '@activeviam/activeui-sdk';
 const Dashboards = () => (
   <Container
     defaultValue={{
-      // TODO: enable "toggle-dock-title-bar" under actions and "add-server" under quickActions to enable the relevant quick action  menu
+      // TODO: Ex-1 enable "toggle-dock-title-bar" under actions and "add-server" under quickActions to enable the relevant quick action  menu
       name: '',
       value: {
         actions: [],
