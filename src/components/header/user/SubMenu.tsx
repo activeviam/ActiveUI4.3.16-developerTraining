99d3b3eb209351b0e367ea5c4f0e5d277ab77b73
Clement Caylux <cca@activeviam.com>
2021-09-07 14:48:53 +0200

    adding the subMenu to add the switch between themes

--- src/components/header/user/SubMenu.tsx
+++ src/components/header/user/SubMenu.tsx
@@ -4,15 +4,22 @@ import Menu from 'antd/lib/menu';
 import {SubMenuProps} from 'antd/lib/menu/SubMenu';
 import Modal from 'antd/lib/modal';
 import Icon from 'antd/lib/icon';
-import {useActiveUI} from '@activeviam/activeui-sdk';
+import {ThemeDescription, useActiveUI} from '@activeviam/activeui-sdk';
 
 const {confirm} = Modal;
+const localeSettingKey = "global.locale";
+const themeSettingKey = "global.theme";
 
 export default function SubMenu(props: SubMenuProps) {
   const activeUI = useActiveUI();
   const username = activeUI.security.getUsername();
   const translator = activeUI.i18n.getTranslator();
-  return (
+
+  const themes: any = activeUI.theming;
+  const settings = activeUI.settings;
+  const i18n  = activeUI.i18n;
+
+    return (
     <Menu.SubMenu {...props} title={<span>{username}</span>}>
       <Menu.Item
         data-testid="MenuItem:logout"
@@ -34,7 +41,20 @@ export default function SubMenu(props: SubMenuProps) {
         {translator.format('bookmarks.dashboard.logout')}
       </Menu.Item>
 
-        // TODO: Ex-2 generate menu items by lopping through themes.listThemes()
+        {/*TODO: Ex-2 generate menu items by lopping through themes.listThemes(). Please note that there is a typo in the ThemeDescription interface and that we should circumvent this by defining the themes as any*/}
+        {themes.listThemes().map((config: any) => (
+            <Menu.Item
+                disabled={!config.selectable}
+                title = {config.value}
+                key = {`theme ${config.value}`}
+                onClick={(valueClicked) => {
+                    settings.set(themeSettingKey, valueClicked.key.slice(6))
+                }}
+            >
+                caption is {config.caption}
+            </Menu.Item>
+        ))}
+
     </Menu.SubMenu>
   );
 }
