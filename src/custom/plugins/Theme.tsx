bcc8299daefc8f0654c6dd4a91d11cb6ae84bbd2
Clement Caylux <cca@activeviam.com>
2021-09-07 10:41:58 +0200

    adding palette and fixing typescript error

--- src/custom/plugins/Theme.tsx
+++ src/custom/plugins/Theme.tsx
@@ -8,12 +8,18 @@
 const themePlugin = {
   key: "custom",
   // returns an object of type ThemeImplementationProperties
-  createProperties(parameters, activeUI) {
+  createProperties(parameters: any, activeUI: any) {
     // 2.1 TODO: Ex-2 Define your own theme.
     // Refer to https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#theme
     // for the available options
     return {
-      // ...
+      isDark: false,
+      palette: {
+        primaryColor: "#FF00FF",
+        errorColor: "#F06292",
+        successColor: "#CDDC39",
+        warningColor: "#FF9800",
+      },
     };
   },
 };
