125cfe09ff575200468863c5a49cf09cd27eee1e
Clement Caylux <cca@activeviam.com>
2021-09-07 09:49:24 +0200

    adding Theme plugin set up

---
+++ src/custom/plugins/Theme.tsx
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * (C) ActiveViam 2019
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ *******************************************************************************/
+
+const themePlugin = {
+  key: "custom",
+  // returns an object of type ThemeImplementationProperties
+  createProperties(parameters, activeUI) {
+    // 2.1 TODO: Ex-2 Define your own theme.
+    // Refer to https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#theme
+    // for the available options
+    return {
+      // ...
+    };
+  },
+};
+
+export default themePlugin;
