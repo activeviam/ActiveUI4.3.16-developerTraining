ba74c630b5b09564fe8030efc96dda2daed5bc70
Clement Caylux <cca@activeviam.com>
2021-09-09 11:06:40 +0200

    adding chart

--- src/custom/dashboards/CustomDashboard.tsx
+++ src/custom/dashboards/CustomDashboard.tsx
@@ -4,6 +4,11 @@
  * property of ActiveViam. Any unauthorized use,
  * reproduction or transfer of this material is strictly prohibited
  *******************************************************************************/
+import React from "react";
+import {Container, ContainerFullValue} from "@activeviam/activeui-sdk";
+import {enhancedPivotTable} from "../widgets/initialWidgets/enhancedPivotTable";
+
+
 
 // =================================================================
 //  Embed dashboard into container for rendering
@@ -15,6 +20,65 @@
 //    TODO: Ex4.4
 //           - add container with childKey myChart, and defaultValue MyChartValue
 //
+
+const myPivotMdx = `SELECT 
+                    NON EMPTY Hierarchize(Hierarchize(DrilldownLevel([CounterParty].[CounterParty].[ALL].[AllMember]))) ON ROWS, 
+                    NON EMPTY Crossjoin([Measures].[pnl.SUM], [Currency].[Currency].[Currency].Members) ON COLUMNS 
+                    FROM [EquityDerivativesCube] 
+                    CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS`;
+
+const MyPivotValue: ContainerFullValue = {
+    name: "Pivot Table",
+    type: "container",
+    value: {
+        style: {},
+        showTitleBar: true,
+        containerKey: "pivot-table",
+        body: {
+            mdx: myPivotMdx,
+            // NOTE: Commenting out the line below will make the pivot table use all default settings instead of our custom ones.
+            configuration: enhancedPivotTable,
+        },
+    },
+    writable: false,
+};
+
+const myChartMdx = `SELECT 
+                    NON EMPTY Crossjoin(
+                      [CounterParty].[CounterParty].[CounterPartyGroup].Members, 
+                      [Currency].[Currency].[Currency].Members
+                    ) ON ROWS, 
+                    NON EMPTY [Measures].[pnl.SUM] ON COLUMNS 
+                    FROM [EquityDerivativesCube]`;
+
+const MyChartValue: ContainerFullValue = {
+    type: "container",
+    writable: false,
+    name: "Chart",
+    value: {
+        showTitleBar: true,
+        containerKey: "chart",
+        body: {
+            configuration: {
+                elementStylers: ["selection-highlight"],
+                // type determines the type of charts to be rendered
+                // in this example, we are using the plotly "100% stacked columns" chart
+                type: "plotly-100-stacked-column-chart",
+                mapping: {
+                    xAxis: ["[CounterParty].[CounterParty].[CounterPartyGroup]"],
+                    values: ["[Measures].[pnl.SUM]"],
+                    stackBy: ["[Currency].[Currency].[Currency]"],
+                },
+            },
+            query: {
+                mdx: myChartMdx,
+                updateMode: "realTime",
+            },
+        },
+    },
+};
+
+
 function MyDashboardComponent() {
     return (
         <div
@@ -25,20 +89,32 @@ function MyDashboardComponent() {
                 flexDirection: "row",
             }}
         >
-            {
-                //...
-            }
+            <div style={{ width: "50%", height: "100%" }}>
+                <Container childKey="myPivotTable" defaultValue={MyPivotValue} />
+            </div>
+            <div style={{ width: "50%" }}>
+                <Container childKey="myChart" defaultValue={MyChartValue} />
+            </div>
         </div>
     );
 }
 
 // 4.1.2 TODO: define a custom plugin that uses the MyDashboardComponent defined in 4.1
 // Refer to https://activeviam.com/activeui/documentation/4.3.11/dev/reference/sdk-api/activeui-sdk.reactcontainerimplementationstaticproperties.html
+// for icons:  https://activeviam.jfrog.io/activeviam/activeui-generic-release/4.3.16/activeui-sdk-showcase-4.3.16.zip!/index.html#/Icons
 const MyCustomDashboardPlugin = {
     key: "myCustomContainer",
     staticProperties: {
-        // ...
+        category: "custom",
+        choosableFromUI: true,
+        iconKey: "menuItem.icon.colors",
+        component: MyDashboardComponent,
+        initialValue: {
+            actions: ["remove-dock", "toggle-dock-title-bar", "clear-dock"],
+            containerKey: "myCustomContainer" // container key must match the plugin key
+        }
     },
 };
 
 // 4.1.3 TODO: export the container plugin by default
+export default MyCustomDashboardPlugin;
