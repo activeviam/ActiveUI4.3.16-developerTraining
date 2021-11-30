75a4b9bbd9a819ec1fd101e5b594962d866c738c
Clement Caylux <cca@activeviam.com>
2021-11-30 19:34:43 +0100

    rearranging

--- src/custom/plugins/SingleValueAction.tsx
+++ src/custom/plugins/SingleValueAction.tsx
@@ -5,7 +5,8 @@
 // execute: use console.log to print out the object actionPayload
 // set debugger in each property step before the function return
 // refer to https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#implement-a-custom-action-plugin
-import {ActionPayload, ActiveUI, TabularHandlerActionPayload} from "@activeviam/activeui-sdk";
+import {ActionPayload, ActiveUI, TabularHandlerActionPayload, Container} from "@activeviam/activeui-sdk";
+import React from "react";
 
 const cityHierarchy = "[Geography].[City]";
 const currencyHierarchy = "[Currency].[Currency]";
@@ -26,6 +27,51 @@ const getCityCurrencyFromPayload = (payload: any, activeUI: ActiveUI) => {
         }
     }
 }
+
+
+
+function SingleValueContent(props: any) {
+    const { mdx } = props;
+    return (
+        <div style={{ width: "100%", height: "120px" }}>
+        </div>
+    );
+}
+
+
+// TODO 5.3: adjust getSingleValueMdx to make use of the city and currency values
+const getSingleValueMdx = (cellValue: any) => `
+SELECT
+NON EMPTY Hierarchize(
+  Crossjoin(
+    DrilldownLevel(
+      [Currency].[Currency].[ALL].[AllMember]
+    ),
+    [Geography].[City].[City].Members
+  )
+) ON ROWS,
+NON EMPTY [Measures].[pnl.SUM] ON COLUMNS,
+{
+  [Booking].[Desk].[ALL].[AllMember].[LegalEntityA].[BusinessUnitA].[DeskA],
+  [Booking].[Desk].[ALL].[AllMember].[LegalEntityA].[BusinessUnitB].[DeskB]
+} ON PAGES
+FROM (
+  SELECT
+  [Geography].[City].[ALL].[AllMember].[${cellValue.cityCaption}] ON COLUMNS
+
+FROM (
+    SELECT
+    [Currency].[Currency].[ALL].[AllMember].[${cellValue.currencyCaption}] ON COLUMNS
+    FROM (
+      SELECT
+        [Measures].[pnl.SUM]
+       ON COLUMNS
+      FROM [EquityDerivativesCube]
+    )
+  )
+)
+CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS`;
+
 
 export const showSingleValue = {
     key: "show-single-value",
@@ -53,7 +99,6 @@ export const showSingleValue = {
         },
         execute(event: React.SyntheticEvent, actionPayload: ActionPayload) {
             // TODO 5.3: use function created to obtain city and currency values
-            // TODO 5.3: adjust getSingleValueMdx to make use of the city and currency values
 
             debugger;
           console.log("actionPayload", actionPayload);
