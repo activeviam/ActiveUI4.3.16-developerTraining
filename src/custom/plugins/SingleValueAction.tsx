c31c95316b12d9631ec005ae0d3a88fbeca4582e
Clement Caylux <cca@activeviam.com>
2021-11-30 18:04:51 +0100

    implementing Single value action

--- src/custom/plugins/SingleValueAction.tsx
+++ src/custom/plugins/SingleValueAction.tsx
@@ -5,11 +5,29 @@
 // execute: use console.log to print out the object actionPayload
 // set debugger in each property step before the function return
 // refer to https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#implement-a-custom-action-plugin
+import {ActionPayload, ActiveUI} from "@activeviam/activeui-sdk";
+
 export const showSingleValue = {
     key: "show-single-value",
-    createProperties(parameters, activeUI) {
+    createProperties(parameters: any, activeUI: ActiveUI) {
         return {
-            // ...
-        };
+        isAvailable(actionPayload: ActionPayload) {
+          debugger;
+          return true;
+        },
+
+        getCaption( actionPayload : ActionPayload) {
+          debugger;
+          return { textPath: "showPnLDiffBetweenDesks" };
+        },
+        getIconSrcKey(actionPayload: ActionPayload) {
+          debugger;
+          return 'menuItem.icon.dockInLegend';
+        },
+        execute(event: React.SyntheticEvent, actionPayload: ActionPayload) {
+          debugger;
+          console.log("actionPayload", actionPayload);
+        }
+      };
     },
 };
