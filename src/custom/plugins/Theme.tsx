8d355148af7b81b4413b429663ad42d5e081f8f0
Clement Caylux <cca@activeviam.com>
2021-12-07 15:15:44 +0100

    removing theme

--- src/custom/plugins/Theme.tsx
+++ src/custom/plugins/Theme.tsx
@@ -6,21 +6,12 @@
  *******************************************************************************/
 
 const themePlugin = {
-  key: "erste",
+  key: "custom",
   // returns an object of type ThemeImplementationProperties
   createProperties(parameters: any, activeUI: any) {
     // 2.1 TODO: Ex-2 Define your own theme.
     // Refer to https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#theme
     // for the available options
-    return {
-      isDark: false,
-      palette: {
-        primaryColor: "#ff0000",
-        errorColor: "#F06292",
-        successColor: "#CDDC39",
-        warningColor: "#FF9800",
-      },
-    };
   },
 };
 
