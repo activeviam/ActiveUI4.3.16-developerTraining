0a06276eb74b49f02fdd11ccd01db61f9083fcee
Clement Caylux <cca@activeviam.com>
2021-12-08 17:34:52 +0100

    adding code for Modal

--- src/custom/plugins/SingleValueAction.tsx
+++ src/custom/plugins/SingleValueAction.tsx
@@ -27,31 +27,78 @@ const getCityCurrencyFromPayload = (payload: any, activeUI: ActiveUI) => {
     }
 }
 
+
+
+function SingleValueContent(props: any) {
+    const { mdx } = props;
+    return (
+        <div style={{ width: "100%", height: "120px" }}>
+        </div>
+    );
+}
+
+
+// TODO 5.3: use getSingleValueMdx to make use of the city and currency values
 export const showSingleValue = {
     key: "show-single-value",
     createProperties(parameters: any, activeUI: ActiveUI) {
         return {
-        isAvailable(actionPayload: ActionPayload) {
-            //TODO: 5.2 destructure the actionPayload to extract actionSituation, widgetApi and context
-            // assert if the action situation is "tabular-handler"
-            // check if the widgetApi datasource uses mdx
-            // check that the context is defined
-            // check that currency and city are available in the payload
+            isAvailable(actionPayload: ActionPayload) {
+                //TODO: 5.2 destructure the actionPayload to extract actionSituation, widgetApi and context
+                // assert if the action situation is "tabular-handler"
+                // check if the widgetApi datasource uses mdx
+                // check that the context is defined
+                // check that currency and city are available in the payload
 
-            //TODO: 5.1 Make the action always available
-          return false;
-        },
+                //TODO: 5.1 Make the action always available
+                return false;
+            },
 
-        getCaption( actionPayload : ActionPayload) {
-          return { textPath: "custom action" };
-        },
-        getIconSrcKey(actionPayload: ActionPayload) {
-          return 'menuItem.icon.dockInLegend';
-        },
-        execute(event: React.SyntheticEvent, actionPayload: ActionPayload) {
+            getCaption( actionPayload : ActionPayload) {
+                return { textPath: "custom action" };
+            },
+            getIconSrcKey(actionPayload: ActionPayload) {
+                return 'menuItem.icon.dockInLegend';
+            },
+            execute(event: React.SyntheticEvent, actionPayload: ActionPayload) {
 
-            //TODO: 5.1 : log the payload in the console
-        }
-      };
+                //TODO: 5.1 : log the payload in the console
+                //TODO: 5.3 : Display a modal with Hello World
+
+            }
+        };
     },
 };
+
+// TODO 5.3: use getSingleValueMdx to make use of the city and currency values
+const getSingleValueMdx = (cellValue: any) => `
+SELECT
+NON EMPTY Hierarchize(
+  Crossjoin(
+    DrilldownLevel(
+      [Currency].[Currency].[ALL].[AllMember]
+    ),
+    [Geography].[City].[City].Members
+  )
+) ON ROWS,
+NON EMPTY [Measures].[pnl.SUM] ON COLUMNS,
+{
+  [Booking].[Desk].[ALL].[AllMember].[LegalEntityA].[BusinessUnitA].[DeskA],
+  [Booking].[Desk].[ALL].[AllMember].[LegalEntityA].[BusinessUnitB].[DeskB]
+} ON PAGES
+FROM (
+  SELECT
+  [Geography].[City].[ALL].[AllMember].[${cellValue.cityCaption}] ON COLUMNS
+
+FROM (
+    SELECT
+    [Currency].[Currency].[ALL].[AllMember].[${cellValue.currencyCaption}] ON COLUMNS
+    FROM (
+      SELECT
+        [Measures].[pnl.SUM]
+       ON COLUMNS
+      FROM [EquityDerivativesCube]
+    )
+  )
+)
+CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS`;
