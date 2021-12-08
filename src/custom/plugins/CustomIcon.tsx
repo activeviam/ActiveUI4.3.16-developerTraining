31575f2fc5226c27dfc95b7be3897cb58405211d
Clement Caylux <cca@activeviam.com>
2021-12-08 19:22:43 +0100

    adding custom icon

---
+++ src/custom/plugins/CustomIcon.tsx
@@ -0,0 +1,12 @@
+// 7.1.1: TODO: create your own custom icon using svg representation
+// refer to showcase example > Smiley Container
+export const iconPlugin = {
+    key: 'my-custom-icon',
+    createProperties() {
+        return {
+            render(palette, props) {
+                // ...
+            }
+        };
+    }
+};
