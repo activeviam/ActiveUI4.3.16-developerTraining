d450f5846cdb0b950e6c3d6ff2bcd15a8eadb8f8
Clement Caylux <cca@activeviam.com>
2021-12-08 17:52:01 +0100

    adding files

---
+++ src/custom/plugins/CountKpiRenderer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// 5.4.1 TODO: create a cell-renderer plugin that returns sets color as follows:
+// cell value < 20 : Red
+// cell value > 40 : Green
+// Otherwise: Orange
+// refer to:
+// showcase example > Cell Renderer
+// https://activeviam.com/activeui/documentation/4.3.16/dev/reference/plugins.html#implement-a-custom-cell-renderer-plugin
+export const countKpi = {
+  key: "count-kpi",
+  createProperties(parameters, activeUI) {
+    // ...
+  },
+};
